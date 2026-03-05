b214c3c33243f6b025146e670f1639fa64b38c6c
kantmanav <kantmanav@gmail.com>
2026-03-04 21:35:20 -0800

    Implement first pass of universal quantifier walker

---
+++ pysmt/walkers/nat_var_lift_dag.py
@@ -0,0 +1,24 @@
+from pysmt.walkers.identitydag import IdentityDagWalker
+from pysmt.typing import INT, NAT
+
+class NatVarLiftDagWalker(IdentityDagWalker):
+    def __init__(self, env=None, invalidate_memoization=None):
+        IdentityDagWalker.__init__(self,
+                           env=env,
+                           invalidate_memoization=invalidate_memoization)
+        self.mgr = self.env.formula_manager
+
+    def walk_forall(self, formula, args, **kwargs):
+        qvars = []
+        guards = []
+        for v in formula.quantifier_vars():
+            if v.symbol_type() is NAT:
+                v_new = self.mgr.Symbol(v.symbol_name(), INT)
+                qvars.append(v_new)
+                guards.append(self.walk_le(None, [0, v_new]))
+            else:
+                qvars.append(v)
+        if not guards:
+            return self.mgr.ForAll(qvars, args[0])
+        else:
+            return self.mgr.Forall(qvars, [self.walk_implies(None, [self.walk_and(None, guards), args[0]])])
