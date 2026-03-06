a5d1cd5ef1ce5935646068ef9d6cdb32535c53ac
kantmanav <kantmanav@gmail.com>
2026-03-06 08:44:39 -0800

    Generalize nat guards function for any variables rather than just quantifier variables

--- pysmt/walkers/nat_var_lift_dag.py
+++ pysmt/walkers/nat_var_lift_dag.py
@@ -8,31 +8,31 @@ class NatVarLiftDagWalker(IdentityDagWalker):
                            invalidate_memoization=invalidate_memoization)
         self.mgr = self.env.formula_manager
 
-    def get_nat_guards(self, formula):
-        qvars = set(formula.quantifier_vars())
-        qvar_subs = {}
+    def get_nat_guards(self, vars):
+        vars = set(vars)
+        var_subs = {}
         guards = []
-        for v in qvars:
+        for v in vars:
             if v.symbol_type() is NAT:
                 v_new = self.mgr.Symbol(v.symbol_name() + "'", INT)
-                qvar_subs[v] = v_new
+                var_subs[v] = v_new
                 guards.append(self.walk_le(None, [self.mgr.Int(0), v_new]))
-        for v, v_new in qvar_subs.items():
-            qvars.remove(v)
-            qvars.add(v_new)
-        return qvars, qvar_subs, guards
+        for v, v_new in var_subs.items():
+            vars.remove(v)
+            vars.add(v_new)
+        return list(vars), var_subs, guards
 
     def walk_forall(self, formula, args, **kwargs):
-        qvars, qvar_subs, guards = self.get_nat_guards(formula)
-        qvars = list(qvars)
+        qvars = formula.quantifier_vars()
+        qvars, qvar_subs, guards = self.get_nat_guards(qvars)
         if not guards:
             return self.mgr.ForAll(qvars, args[0])
         else:
             return self.mgr.ForAll(qvars, self.walk_implies(None, [self.walk_and(None, guards), args[0].substitute(qvar_subs)]))
 
     def walk_exists(self, formula, args, **kwargs):
-        qvars, qvar_subs, guards = self.get_nat_guards(formula)
-        qvars = list(qvars)
+        qvars = formula.quantifier_vars()
+        qvars, qvar_subs, guards = self.get_nat_guards(qvars)
         if not guards:
             return self.mgr.Exists(qvars, args[0])
         else:
